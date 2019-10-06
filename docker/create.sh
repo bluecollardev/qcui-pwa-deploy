c90849181e7402ebd8a2777b0b03846e38e4f675
Lucas <tech@firebrandwebsolutions.com>
2019-10-06 16:54:50 -0700

    Update create script.

--- docker/create.sh
+++ docker/create.sh
@@ -10,18 +10,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ### Configure paths
 WS_PATH="${DIR}/../app"
 
-# Clone the app
-git clone --depth 1 https://github.com/bluecollardev/qcui-pwa-demo.git ${WS_PATH}
-
-# Zap .git, it's not needed for the output image(s)
+rm -rf ${WS_PATH}/*
+# Delete any old .git stuff
 rm -rf $(find ${WS_PATH}/. -name ".git" -or -name ".gitignore" -or -name ".gitattributes")
 
-# Set cwd to frontend dir
-cd ${WS_PATH}
+# Clone the app
+git clone --depth 1 https://github.com/bluecollardev/qcui-pwa-demo.git ${WS_PATH}
+# Zap .git, it's not needed for the output image(s)
+rm -rf $(find ${WS_PATH} -name ".git" -or -name ".gitignore" -or -name ".gitattributes")
 
 # Update file permissions
 echo "Update permissions"
 chown -R ${USER:=$(/usr/bin/id -run)} ${WS_PATH}
+
+# Set cwd to original dir
+cd ${WS_PATH}
 
 # Build frontend
 npm install &&
